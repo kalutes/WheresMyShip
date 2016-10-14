21560d3434f1aee5ae0e88c9dcc2c1acde61fd62
Kameron Lutes <Kameron Lutes>
2016-10-14 12:59:52 -0400

    Added auto stop grabbing after 3 days old emails and other stuff

--- HTMLEmailGrabber.php
+++ HTMLEmailGrabber.php
@@ -16,6 +16,21 @@ if (php_sapi_name() != 'cli') {
   throw new Exception('This application must be run on the command line.');
 }
 
+$client = getClient();
+$gmail = new Google_Service_Gmail($client);
+mkdir('messages');
+chdir('messages');
+$newestDate = 0;
+
+
+class Email {
+	public $From;
+	public $To;
+	public $Subject;
+	public $Date;
+	public $HTMLBody;
+}
+
 /**
  * Returns an authorized API client.
  * @return Google_Client the authorized client object
@@ -104,8 +119,7 @@ function expandHomeDirectory($path) {
 }
 
 // Get the API client and construct the service object.
-$client = getClient();
-$gmail = new Google_Service_Gmail($client);
+
 
 //$messages = listMessages($service, 'me');
 
@@ -122,161 +136,10 @@ function decodeBody($body) {
     return $decodedMessage;
 }
 
-$list = $gmail->users_messages->listUsersMessages('me', ['maxResults' => 1000]);
-$i = 0;
-mkdir('messages');
-chdir('messages');
-$date;
-try{
-    while ($list->getMessages() != null) {
-
-        foreach ($list->getMessages() as $mlist) {
-
-            $message_id = $mlist->id;
-            $optParamsGet2['format'] = 'full';
-            $single_message = $gmail->users_messages->get('me', $message_id, $optParamsGet2);
-			$date = $single_message->getInternalDate() / 1000;
-			$dt = new DateTime("@$date");
-			$date = $dt->format('Y-m-d H-i-s');
-            $payload = $single_message->getPayload();
-            $parts = $payload->getParts();
-            // With no attachment, the payload might be directly in the body, encoded.
-            $body = $payload->getBody();
-            $FOUND_BODY = FALSE;
-            // If we didn't find a body, let's look for the parts
-            if(!$FOUND_BODY) {
-                foreach ($parts  as $part) {
-                    if($part['parts'] && !$FOUND_BODY) {
-                        foreach ($part['parts'] as $p) {
-                            if($p['parts'] && count($p['parts']) > 0){
-                                foreach ($p['parts'] as $y) {
-                                    if(($y['mimeType'] === 'text/html') && $y['body']) {
-                                        $FOUND_BODY = decodeBody($y['body']->data);
-                                        break;
-                                    }
-                                }
-                            } else if(($p['mimeType'] === 'text/html') && $p['body']) {
-                                $FOUND_BODY = decodeBody($p['body']->data);
-                                break;
-                            }
-                        }
-                    }
-                    if($FOUND_BODY) {
-                        break;
-                    }
-                }
-            }
-            // let's save all the images linked to the mail's body:
-            if($FOUND_BODY && count($parts) > 1){
-                $images_linked = array();
-                foreach ($parts  as $part) {
-                    if($part['filename']){
-                        array_push($images_linked, $part);
-                    } else{
-                        if($part['parts']) {
-                            foreach ($part['parts'] as $p) {
-                                if($p['parts'] && count($p['parts']) > 0){
-                                    foreach ($p['parts'] as $y) {
-                                        if(($y['mimeType'] === 'text/html') && $y['body']) {
-                                            array_push($images_linked, $y);
-                                        }
-                                    }
-                                } else if(($p['mimeType'] !== 'text/html') && $p['body']) {
-                                    array_push($images_linked, $p);
-                                }
-                            }
-                        }
-                    }
-                }
-                // special case for the wdcid...
-                preg_match_all('/wdcid(.*)"/Uims', $FOUND_BODY, $wdmatches);
-                if(count($wdmatches)) {
-                    $z = 0;
-                    foreach($wdmatches[0] as $match) {
-                        $z++;
-                        if($z > 9){
-                            $FOUND_BODY = str_replace($match, 'image0' . $z . '@', $FOUND_BODY);
-                        } else {
-                            $FOUND_BODY = str_replace($match, 'image00' . $z . '@', $FOUND_BODY);
-                        }
-                    }
-                }
-                preg_match_all('/src="cid:(.*)"/Uims', $FOUND_BODY, $matches);
-                if(count($matches)) {
-                    $search = array();
-                    $replace = array();
-                    // let's trasnform the CIDs as base64 attachements 
-                    foreach($matches[1] as $match) {
-                        foreach($images_linked as $img_linked) {
-                            foreach($img_linked['headers'] as $img_lnk) {
-                                if( $img_lnk['name'] === 'Content-ID' || $img_lnk['name'] === 'Content-Id' || $img_lnk['name'] === 'X-Attachment-Id'){
-                                    if ($match === str_replace('>', '', str_replace('<', '', $img_lnk->value)) 
-                                            || explode("@", $match)[0] === explode(".", $img_linked->filename)[0]
-                                            || explode("@", $match)[0] === $img_linked->filename){
-                                        $search = "src=\"cid:$match\"";
-                                        $mimetype = $img_linked->mimeType;
-                                        $attachment = $gmail->users_messages_attachments->get('me', $mlist->id, $img_linked['body']->attachmentId);
-                                        $data64 = strtr($attachment->getData(), array('-' => '+', '_' => '/'));
-                                        $replace = "src=\"data:" . $mimetype . ";base64," . $data64 . "\"";
-                                        $FOUND_BODY = str_replace($search, $replace, $FOUND_BODY);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            // If we didn't find the body in the last parts, 
-            // let's loop for the first parts (text-html only)
-            if(!$FOUND_BODY) {
-                foreach ($parts  as $part) {
-                    if($part['body'] && $part['mimeType'] === 'text/html') {
-                        $FOUND_BODY = decodeBody($part['body']->data);
-                        break;
-                    }
-                }
-            }
-            // With no attachment, the payload might be directly in the body, encoded.
-            if(!$FOUND_BODY) {
-                $FOUND_BODY = decodeBody($body['data']);
-            }
-            // Last try: if we didn't find the body in the last parts, 
-            // let's loop for the first parts (text-plain only)
-            if(!$FOUND_BODY) {
-                foreach ($parts  as $part) {
-                    if($part['body']) {
-                        $FOUND_BODY = decodeBody($part['body']->data);
-                        break;
-                    }
-                }
-            }
-            if(!$FOUND_BODY) {
-                $FOUND_BODY = '(No message)';
-            }
-            // Finally, print the message ID and the body
-            print_r($message_id . "\r\n");
-			file_put_contents($date . " " . $i . '.html', $FOUND_BODY);
-			$i++;
-        }
-
-        if ($list->getNextPageToken() != null) {
-            $pageToken = $list->getNextPageToken();
-            $list = $gmail->users_messages->listUsersMessages('me', ['pageToken' => $pageToken, 'maxResults' => 1000]);
-        } else {
-            break;
-        }
-    }
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
-
 /*Returns an array of headers: To, From, Subject, and Date for a message. Access member vars by typing $headers->Subject for example*/
-function getHeaders($service,$userId,$messageId){
+function getOurHeaders($fullEmail){
     $headers = (object)array('From' => NULL, 'To' => NULL, 'Subject' => NULL, 'Date' => NULL);
-    $opt_param_get = array();
-    $opt_param_get['format'] = 'full';
-    $message = $service->users_messages->get($userId,$messageId,$opt_param_get);
-    $headersArray = $message->getPayload()->getHeaders();
+    $headersArray = $fullEmail->getPayload()->getHeaders();
     foreach($headersArray as $entry){
         if($entry->name == "From"){
             $headers->From = $entry->value;
@@ -288,15 +151,197 @@ function getHeaders($service,$userId,$messageId){
             $headers->Date = $entry->value;
         } 
     }
-    return $headers;    
-    
+    return $headers;        
 }
 
-class Email {
-	public $From;
-	public $To;
-	public $Subject;
-	public $Date;
-	public $HTMLBody;
+
+
+
+function getNewEmails($initialGrab = false)
+{
+	global $gmail;
+	$list = $gmail->users_messages->listUsersMessages('me', ['maxResults' => 1000]);
+	$i = 0;
+	$date;
+	global $newestDate;
+	$continueGrabbingEmails = true;
+	$currentDate = time();
+	try{
+		while ($list->getMessages() != null && $continueGrabbingEmails) {
+
+			foreach ($list->getMessages() as $mlist) {
+
+				$message_id = $mlist->id;
+				$optParamsGet2['format'] = 'full';
+				$single_message = $gmail->users_messages->get('me', $message_id, $optParamsGet2);
+				$date = $single_message->getInternalDate() / 1000;
+				if($initialGrab && $currentDate - $date > (3*24*3600))
+				{
+					$continueGrabbingEmails = false;
+					break;
+				}
+				if(!$initialGrab && $newestDate >= $date)
+				{
+					$continueGrabbingEmails = false;
+					break;
+				}
+				if($date > $newestDate)
+				{
+					$newestDate = $date;
+					print_r("Found New Email!\r\n");
+				}
+				$payload = $single_message->getPayload();
+				$parts = $payload->getParts();
+				// With no attachment, the payload might be directly in the body, encoded.
+				$body = $payload->getBody();
+				$FOUND_BODY = FALSE;
+				// If we didn't find a body, let's look for the parts
+				if(!$FOUND_BODY) {
+					foreach ($parts  as $part) {
+						if($part['parts'] && !$FOUND_BODY) {
+							foreach ($part['parts'] as $p) {
+								if($p['parts'] && count($p['parts']) > 0){
+									foreach ($p['parts'] as $y) {
+										if(($y['mimeType'] === 'text/html') && $y['body']) {
+											$FOUND_BODY = decodeBody($y['body']->data);
+											break;
+										}
+									}
+								} else if(($p['mimeType'] === 'text/html') && $p['body']) {
+									$FOUND_BODY = decodeBody($p['body']->data);
+									break;
+								}
+							}
+						}
+						if($FOUND_BODY) {
+							break;
+						}
+					}
+				}
+				// let's save all the images linked to the mail's body:
+				if($FOUND_BODY && count($parts) > 1){
+					$images_linked = array();
+					foreach ($parts  as $part) {
+						if($part['filename']){
+							array_push($images_linked, $part);
+						} else{
+							if($part['parts']) {
+								foreach ($part['parts'] as $p) {
+									if($p['parts'] && count($p['parts']) > 0){
+										foreach ($p['parts'] as $y) {
+											if(($y['mimeType'] === 'text/html') && $y['body']) {
+												array_push($images_linked, $y);
+											}
+										}
+									} else if(($p['mimeType'] !== 'text/html') && $p['body']) {
+										array_push($images_linked, $p);
+									}
+								}
+							}
+						}
+					}
+					// special case for the wdcid...
+					preg_match_all('/wdcid(.*)"/Uims', $FOUND_BODY, $wdmatches);
+					if(count($wdmatches)) {
+						$z = 0;
+						foreach($wdmatches[0] as $match) {
+							$z++;
+							if($z > 9){
+								$FOUND_BODY = str_replace($match, 'image0' . $z . '@', $FOUND_BODY);
+							} else {
+								$FOUND_BODY = str_replace($match, 'image00' . $z . '@', $FOUND_BODY);
+							}
+						}
+					}
+					preg_match_all('/src="cid:(.*)"/Uims', $FOUND_BODY, $matches);
+					if(count($matches)) {
+						$search = array();
+						$replace = array();
+						// let's trasnform the CIDs as base64 attachements 
+						foreach($matches[1] as $match) {
+							foreach($images_linked as $img_linked) {
+								foreach($img_linked['headers'] as $img_lnk) {
+									if( $img_lnk['name'] === 'Content-ID' || $img_lnk['name'] === 'Content-Id' || $img_lnk['name'] === 'X-Attachment-Id'){
+										if ($match === str_replace('>', '', str_replace('<', '', $img_lnk->value)) 
+												|| explode("@", $match)[0] === explode(".", $img_linked->filename)[0]
+												|| explode("@", $match)[0] === $img_linked->filename){
+											$search = "src=\"cid:$match\"";
+											$mimetype = $img_linked->mimeType;
+											$attachment = $gmail->users_messages_attachments->get('me', $mlist->id, $img_linked['body']->attachmentId);
+											$data64 = strtr($attachment->getData(), array('-' => '+', '_' => '/'));
+											$replace = "src=\"data:" . $mimetype . ";base64," . $data64 . "\"";
+											$FOUND_BODY = str_replace($search, $replace, $FOUND_BODY);
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+				// If we didn't find the body in the last parts, 
+				// let's loop for the first parts (text-html only)
+				if(!$FOUND_BODY) {
+					foreach ($parts  as $part) {
+						if($part['body'] && $part['mimeType'] === 'text/html') {
+							$FOUND_BODY = decodeBody($part['body']->data);
+							break;
+						}
+					}
+				}
+				// With no attachment, the payload might be directly in the body, encoded.
+				if(!$FOUND_BODY) {
+					$FOUND_BODY = decodeBody($body['data']);
+				}
+				// Last try: if we didn't find the body in the last parts, 
+				// let's loop for the first parts (text-plain only)
+				if(!$FOUND_BODY) {
+					foreach ($parts  as $part) {
+						if($part['body']) {
+							$FOUND_BODY = decodeBody($part['body']->data);
+							break;
+						}
+					}
+				}
+				if(!$FOUND_BODY) {
+					$FOUND_BODY = '(No message)';
+				}
+				// Finally, print the message ID and the body
+				print_r($message_id . "\r\n");
+				if($mlist != null)
+				{
+					$headers = getOurHeaders($single_message);
+					$email = new Email;
+					$email->To = $headers->To;
+					$email->From = $headers->From;
+					$email->Subject = $headers->Subject;
+					$email->Date = $headers->Date;
+					$email->HTMLBody = $FOUND_BODY;
+					//ADD CODE HERE TO SAVE TO DATABASE ONCE ITS CONSTRUCTED
+					file_put_contents($date . " " . $i . '.html', $FOUND_BODY);
+					$i++;
+				}
+			}
+
+			if ($list->getNextPageToken() != null) {
+				$pageToken = $list->getNextPageToken();
+				$list = $gmail->users_messages->listUsersMessages('me', ['pageToken' => $pageToken, 'maxResults' => 1000]);
+			} else {
+				break;
+			}
+		}
+	} catch (Exception $e) {
+		echo $e->getMessage();
+	}
 }
 
+$timerDate = time();
+getNewEmails(true);
+print_r("Waiting on new Emails...\r\n");
+while(true)
+{
+	if(time() - $timerDate >= 5)
+	{
+		$timerDate = time();
+		getNewEmails();
+	}
+}
