2d918435d80e8174a7fb74e291b640c4e5633322
Wilson E. Husin <wilsonehusin@gmail.com>
2016-10-17 04:03:00 -0400

    PotentialEmails.py uses pythonfiles/target.txt for reference. For every run, the python script will now iterate through a folder instead of a file.

--- PotentialEmails.py
+++ PotentialEmails.py
@@ -2,18 +2,22 @@ import sys
 import os
 
 def usage():
-    print "Usage: python %s [filename]" % sys.argv[0]
+    print "Usage: python %s [folder name]" % sys.argv[0]
 
-def keywordLookup(lines):
+def loadtargets():
     x = 0
     words = []
-    fd = open("words.txt", "r")
+    fd = open("pythonfiles/target.txt", "r")
     while True:
         words.append((fd.readline())[0:-1])
         if words[x] == '':
             del(words[x])
             break
         x += 1
+    return words
+
+def keywordLookup(lines, ref):
+    words = ref
     count = 0
     for n in xrange(0, len(lines)):
         org = lines[n]
@@ -23,29 +27,36 @@ def keywordLookup(lines):
             for i in xrange(0, len(line)):
                 current = line.find(words[x])
                 if current > -1:
+                    print "Found " + words[x] + "in " + line
                     count += 1
                     line = line[(current + 1):len(line)]
                 else:
                     break
-
     return count
 
 def main():
     if len(sys.argv) != 2:
         usage()
         return
-    fd = open(sys.argv[1], 'r')
-    buff = []
-    tempbuff = fd.readline()
-    tempbuff = tempbuff[0:-1]
-    while tempbuff != '':
-        buff.append(tempbuff.lower())
-        tempbuff = fd.readline()
-    fd.closed
-    score = keywordLookup(buff)
-    filename = str(score) + "_" + sys.argv[1]
-    os.system("mkdir -p ./priority")
-    os.system("mv " + sys.argv[1] + " ./priority/" + filename)
+    ref = loadtargets()
+    targetdir = "./" + sys.argv[1] + "/"
+    os.chdir(targetdir)
+    for ls in os.listdir("./"):
+        if os.path.isfile(ls):
+            fd = open(ls ,'r')
+            buff = []
+            tempbuff = fd.readline()
+            if tempbuff[len(tempbuff) - 1] == '\n':
+                tempbuff = tempbuff[0:-1]
+            while tempbuff != '':
+                buff.append(tempbuff.lower())
+                tempbuff = fd.readline()
+            fd.closed
+            score = keywordLookup(buff, ref)
+            filename = "\"" + str(score) + "_" + ls + "\""
+            complete = "\"" + ls + "\""
+            os.system("mkdir -p ../priority")
+            os.system("mv " + complete + " ../priority/" + filename)
     return
 
 if __name__ == "__main__":
