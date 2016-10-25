bc64ea958b8329928a2c7de315dc78d6e8940f5a
Wilson E. Husin <wilsonehusin@gmail.com>
2016-10-25 16:17:42 -0400

    PotentialEmails.php code structure is ready.

--- PotentialEmails.php
+++ PotentialEmails.php
@@ -4,9 +4,27 @@
 	}
 	chdir('messages');
 	$dir = new DirectoryIterator(__DIR__);
+	if (!file_exists('confirmed')) {
+		mkdir('confirmed');
+	}
+	if (!file_exists('maybe')) {
+		mkdir('maybe');
+	}
+	if (!file_exists('nonrelated')) {
+		mkdir('nonrelated');
+	}
 	foreach ($dir as $filedesc) {
-			if (!$filedesc->isDot() && file_exists($filedesc->getFilename())) {
-
+		if (!$filedesc->isDot() && file_exists($filedesc->getFilename())) {
+			if (/* Subject contains target phrases */) {
+				// Move email to confirmed folder
+				rename($filedesc->getFilename(), 'confirmed/'.($filedesc->getFilename()));
+			} else if (/* Has a high score of target words */) {
+				// Move email to maybe folder
+				rename($filedesc->getFilename(), 'maybe/'.($filedesc->getFilename()));
+			} else {
+				// Move email to nonrelated folder
+				rename($filedesc->getFilename(), 'nonrelated'.($filedesc->getFilename()));
 			}
+		}
 	}
 ?>
