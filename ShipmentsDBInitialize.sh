39cd12cbbaa323ced3258a40f149d9acbe5aca52
chen1860 <chen1860@data.cs.purdue.edu>
2016-11-30 21:15:07 -0500

    fixed issue with localhost and error promptiong

--- ShipmentsDBInitialize.sh
+++ ShipmentsDBInitialize.sh
@@ -1,3 +1,4 @@
+ mysql_config_editor set --login-path=local --host=localhost --user=userfrosting_adm --password
 if [ $(mysql --login-path=local -N -s -e "select count(*) from userfrosting.uf_user") -eq 1 ]; then
 mysql -u 'userfrosting_adm' --password='wheresmyship' -e "create table uf_shipments(id int NOT NULL auto_increment, userid int, trackingNumber varchar(50), primary key(id));"
 else
