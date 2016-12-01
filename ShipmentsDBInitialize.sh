11e219833bb37870b5c7cb000c17aed2dda83231
Matthew <mashbeck@purdue.edu>
2016-11-30 20:01:06 -0500

    Added queries to modify uf_user to script. I love Akari

--- ShipmentsDBInitialize.sh
+++ ShipmentsDBInitialize.sh
@@ -2,4 +2,6 @@ mysql -u 'userfrosting_adm' --password='wheresmyship' << EOF
 use userfrosting;
 
 create table uf_shipments(id int NOT NULL auto_increment, userid int, trackingNumber varchar(50), primary key(id));
-
+alter table uf_user add googleauth varchar(1000);
+alter table uf_user add firstgrab int(1) not null default 1;
+alter table uf_user add lastemaildate int(30) not null default 0;
