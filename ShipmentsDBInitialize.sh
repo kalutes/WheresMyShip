1543e69d64cde591611da29a842c234a875a31ad
chen1860 <chen1860@data.cs.purdue.edu>
2016-11-30 19:56:18 -0500

    fixed db script

--- ShipmentsDBInitialize.sh
+++ ShipmentsDBInitialize.sh
@@ -1,1 +1,5 @@
+mysql -u 'userfrosting_adm' --password='wheresmyship' << EOF
+use userfrosting;
+
 create table uf_shipments(id int NOT NULL auto_increment, userid int, trackingNumber varchar(50), primary key(id));
+
