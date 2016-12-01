9dbe505623159619757129fffee0ab588c8eb572
chen1860 <chen1860@data.cs.purdue.edu>
2016-11-30 21:12:45 -0500

    updated script

--- ShipmentsDBInitialize.sh
+++ ShipmentsDBInitialize.sh
@@ -1,7 +1,25 @@
-mysql -u 'userfrosting_adm' --password='wheresmyship' << EOF
-use userfrosting;
+if [ $(mysql --login-path=local -N -s -e "select count(*) from userfrosting.uf_user") -eq 1 ]; then
+mysql -u 'userfrosting_adm' --password='wheresmyship' -e "create table uf_shipments(id int NOT NULL auto_increment, userid int, trackingNumber varchar(50), primary key(id));"
+else
+	echo "table already exists"
+fi
+VAR=$(mysql --login-path=local -N -s  -e "select * from information_schema.COLUMNS where table_schema='userfrosting' and table_name='uf_user' and column_name='googleauth'")
+if [ -z "$VAR" ]; then
+ mysql --login-path=local -e "alter table userfrosting.uf_user add googleauth varchar(1000);"
+else
+	echo "column already exists"
+fi
+VAR=$(mysql --login-path=local -N -s  -e "select * from information_schema.COLUMNS where table_schema='userfrosting' and table_name='uf_user' and column_name='firstgrab'")
+if [ -z "$VAR" ]; then
+ mysql --login-path=local -e "alter table userfrosting.uf_user add firstgrab int(1) not null default 1;"
+else
+	echo "column already exists"
+fi
+VAR=$(mysql --login-path=local -N -s -e "select * from information_schema.COLUMNS where table_schema='userfrosting' and table_name='uf_user' and column_name='lastemaildate'")
+if [ -z "$VAR" ]; then
+ mysql --login-path=local -e "alter table userfrosting.uf_user add lastemaildate int(30) not null default 0;"
+else
+	echo "column already exists"
+fi
 
-create table uf_shipments(id int NOT NULL auto_increment, userid int, trackingNumber varchar(50), primary key(id));
-alter table uf_user add googleauth varchar(1000);
-alter table uf_user add firstgrab int(1) not null default 1;
-alter table uf_user add lastemaildate int(30) not null default 0;
+
