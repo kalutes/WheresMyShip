485dc29cb300896ae91b66893be99a893531265c
chen1860 <chen1860@data.cs.purdue.edu>
2016-12-01 01:20:49 -0500

    idk

--- ShipmentsDBInitialize.sh
+++ ShipmentsDBInitialize.sh
@@ -1,12 +1,12 @@
  mysql_config_editor set --login-path=local --host=localhost --user=userfrosting_adm --password
 if [ $(mysql --login-path=local -N -s -e "select count(*) from userfrosting.uf_user") -eq 1 ]; then
-mysql --login-path=local -e "CREATE TABLE userfrosting.uf_shipments (id int NOT NULL AUTO_INCREMENT, userid int, trackingNumber varchar(50),shipDate int, origin varchar(1000),destination varchar(1000), currentLocation varchar(1000), eta int, PRIMARY KEY (id));"
+mysql --login-path=local -e "create table userfrosting.uf_shipments(id int NOT NULL auto_increment, userid int, trackingNumber varchar(50), primary key(id));"
 else
 	echo "table already exists"
 fi
 VAR=$(mysql --login-path=local -N -s  -e "select * from information_schema.COLUMNS where table_schema='userfrosting' and table_name='uf_user' and column_name='googleauth'")
 if [ -z "$VAR" ]; then
- mysql --login-path=local -e "alter table userfrosting.uf_user add googleauth varchar(1000);"
+ mysql --login-path=local -e "alter table userfrosting.uf_user add googleauth text not null;"
 else
 	echo "column already exists"
 fi
@@ -22,5 +22,3 @@ if [ -z "$VAR" ]; then
 else
 	echo "column already exists"
 fi
-
-
